fa8b760ce0a1d761db88c7fc0d9c9a4b971372bb
eq777 <ericqi777@yahoo.com>
2019-08-02 23:51:09 -0700

    first version configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCuPNwq-Tp-R9VnlvDJUWCR5K3NPTw8R30",
+    authDomain: "mexitong-a8144.firebaseapp.com",
+    databaseURL: "https://mexitong-a8144.firebaseio.com",
+    projectId: "mexitong-a8144",
+    storageBucket: "mexitong-a8144.appspot.com",
+    messagingSenderId: "498241001655",
+    appId: "1:498241001655:web:24a04edaa65ca079"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCuPNwq-Tp-R9VnlvDJUWCR5K3NPTw8R30",
+    authDomain: "mexitong-a8144.firebaseapp.com",
+    databaseURL: "https://mexitong-a8144.firebaseio.com",
+    projectId: "mexitong-a8144",
+    storageBucket: "mexitong-a8144.appspot.com",
+    messagingSenderId: "498241001655",
+    appId: "1:498241001655:web:24a04edaa65ca079"
+  }
 };
 
 /*
